0a931aa8b8e8e0c0724ca091fb9c67c83b81e3ea
Ruben <ruben.dewit@gmx.com>
2020-03-26 02:15:19 +0100

    * Final countdown

---
+++ README.md
@@ -0,0 +1,25 @@
+
+# Challenge 2 Mars klok
+> Ruben de Wit // 18054048
+
+# Opdracht
+Bewoners van Mars, de zogenoemde "New Martians", hebben bij Elon Musk geklaagd over het ontbreken van een centrale klok.
+Elon heeft gevraagd een klok te maken dat de aardse tijd weergeeft en visueel maakt welke acties bewoners op een dag moeten doen (slapen, opstaan, enz...).
+Om neerslachtigheid tegen te gaan moet dit op geanimeerde wijze.
+
+# Uitwerking
+Ontwerpkeuzes
+-------------
+Het ontwerp is in de basis circulair om zo ook in de toekomst mogelijk smartwatches te bedienen (hoewel op dit moment smarwatches over het algemeen geen HTML ondersteuning hebben spreken we nu over de toekomst waarin dit waarschijnlijk wel kan).  
+Er is gekozen voor typische Mars kleuren in tinten donker oranje en bruin.
+Wanneer de applicatie in de nacht opereert zal het thema donkerder worden.
+Tegen de neerslachtigheid zijn geanimeerde wolken toegevoegd wat rustgevend moet overkomen.
+Daarnaast moeten de zon en de maan voor meer visuele houvast zorgen. De positie van deze planeten is ook afhankelijk van de tijd.
+
+Om deze applicatie makkelijk te testen is er een knop "Fast Forward" toegevoegd.  
+Deze knop zorgt ervoor dat de klok een etmaal in 1 minuut doorloopt.
+
+Uitdagingen die ik in deze opdracht ben aangegaan zijn:
+* Een klok maken van scratch met JS, HTML en CSS
+* Met CSS clipPath uitsnedes maken in elementen
+* Leren hoe SVG elementen tot stand komen
--- js/clock.js
+++ js/clock.js
@@ -1,14 +1,69 @@
-const hourHand = document.getElementById("clock--hour-hand")
-const minuteHand = document.getElementById("clock--minute-hand")
-function setClock() {
-  const time = new Date()
+const $hourHand = document.getElementById("clock--hour-hand")
+const $minuteHand = document.getElementById("clock--minute-hand")
+const $time = document.getElementById("time--digital")
+const $weekday = document.getElementById("time--weekday")
+const $date = document.getElementById("time--date")
+const $alarm = document.getElementById("time--alarm")
+const $body = document.body
+const $card = document.getElementById("card--background")
+const $sun = document.getElementById("sun")
+const $moon = document.getElementById("moon")
+const $ffBtn = document.getElementById("ffmode")
+
+let ffMode = false
+let timer = setInterval(setClock, 10000)
+
+function getAlarm(hour) {
+  if (hour >= 7 && hour <= 8) return "Opstaan"
+  else if (hour === 9) return "Ontbijt"
+  else if (hour > 9 && hour < 12) return "Sporten"
+  else if (hour === 12) return "Lunch"
+  else if (hour > 12 && hour < 17) return "Werken"
+  else if (hour === 17) return "Diner"
+  else if (hour > 17 && hour <= 20) return "Happy hour"
+  else if (hour >= 21 && hour <= 22) return "TV kijken"
+  else if (hour > 22 && hour <= 23) return "Naar bed"
+  else {
+    return "Slapen"
+  }
+}
+
+function setClock(custom) {
+  const time = custom || new Date()
   const minute = time.getMinutes()
   const second = time.getSeconds()
-  const minutePos = 360 * (minute + second / 60) / 60 - 90
   const hour = time.getHours()
+  const minutePos = 360 * (minute + second / 60) / 60 - 90
+  const leadingMinute = minute.toString().padStart(2, '0')
   const hourPos = 30 * (hour % 12 + minute / 60) - 90
-  hourHand.style.transform = `rotate(${hourPos}deg)`
-  minuteHand.style.transform = `rotate(${minutePos}deg)`
+
+  $hourHand.style.transform = `rotate(${hourPos}deg)`
+  $minuteHand.style.transform = `rotate(${minutePos}deg)`
+  $sun.style.transform = `rotate(${-90 + 360 / 24 * hour}deg)`
+  $moon.style.transform = `rotate(${90 + 360 / 24 * hour}deg)`
+  $body.style.backgroundPosition = $card.style.backgroundPosition = (hour >= 0 && hour < 7) ? "bottom" : "center"
+
+  $time.innerHTML = `${hour}:${leadingMinute}`
+  $weekday.innerHTML = time.toLocaleDateString(undefined, { weekday: 'long' })
+  $date.innerHTML = time.toLocaleDateString(undefined, { month: 'long', day: 'numeric' })
+  $alarm.innerHTML = getAlarm(hour) || ""
 }
-setInterval(setClock, 10000)
-setClock();
+
+function toggleFFMode() {
+  clearInterval(timer)
+  if (ffMode) {
+    timer = setInterval(setClock, 10000)
+    setClock()
+  } else {
+    timer = setInterval(() => {
+      let time = new Date();
+      time.setHours(Math.round(time.getSeconds() / 60 * 24))
+      time.setMinutes(time.getSeconds())
+      setClock(time)
+    }, 1000)
+  }
+  ffMode = !ffMode
+}
+
+setClock()
+$ffBtn.addEventListener("click", toggleFFMode)
